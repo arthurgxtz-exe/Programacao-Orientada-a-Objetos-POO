0e502d7904875211c8d1c717bdd197c27bf613ee
Arthur Almeida <almeida.arthur.ti@gmail.com>
2026-08-06 14:01:20 -0300

    leet code palindromo

--- Exercicios Introdutorios/Ex4.java
+++ Exercicios Introdutorios/Ex4.java
@@ -1,3 +1,21 @@
+import java.util.Random;
 public class Ex4 {
-    
+    public static void main(String[] args){
+        Random gerador = new Random();
+
+        int[] vet = new int[6];
+        int maior = 0;
+        System.out.println("Numeros gerados:");
+        for(int i = 0; i < vet.length; i++){
+            vet[i] = gerador.nextInt(401) + 100;
+            System.out.println(vet[i]);
+            if(vet[i] > maior){
+                maior = vet[i];
+            }
+        }
+
+        System.out.println("O maior valor:"+ maior);
+
+
+    }
 }
---
+++ Exercicios Introdutorios/Palindromo.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Palindromo {
+
+    public static int palindromo(String nome){
+        int tamanho = nome.length();
+        int sinal = 1;
+        
+        for(int i = 0; i < nome.length() / 2; i++ ){
+    
+            if(nome.charAt(i) != nome.charAt(tamanho - 1)){
+                sinal = 0;
+                return sinal;                           
+                
+            }
+            else{
+                tamanho--;
+            }
+        }
+       return sinal;
+    }
+    public static void main(String[] args){
+        Scanner scanf = new Scanner(System.in);
+        String nome;
+        while(true){
+            System.out.println("Digite uma palavra: (ou 0 para sair) ");
+            nome = scanf.nextLine();
+    
+            if(nome.equals("0")){
+                System.out.println("Encerrando programa...");
+                break;
+            }
+            if(palindromo(nome) == 1){
+                System.out.println("Essa string é um palindromo.");
+    
+            }
+            else
+                System.out.println("Essa string não é um palindromo.");
+    
+        }
+        
+        scanf.close();
+    }
+}
